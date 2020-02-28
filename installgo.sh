10d96c6cd0c3822bb7fb3c06a0a19c02057bb419
nope <kaserman22@gmail.com>
2020-02-28 13:30:40 -0500

    go install and update scripts

---
+++ installgo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+wget -P $HOME https://dl.google.com/go/go1.4-bootstrap-20171003.tar.gz 
+tar xzvf $HOME/go1.4-bootstrap-20171003.tar.gz -C $HOME
+mv $HOME/go/ $HOME/go1.4/
+export CGO_ENABLED=0
+cd $HOME/go1.4/src/ && ./make.bash
+git clone https://go.googlesource.com/go $HOME/goroot
+cd $HOME/goroot && git checkout go1.14
+cd $HOME/goroot/src/ && ./all.bash
+sed -i "$ a shopt -s extglob\nexport GOLOC=/home/$USER/goroot/bin\nexport PATH=$PATH:$GOLOC" ~/.bashrc
---
+++ update_go.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd goroot/src && git fetch
+echo 'Run $ git checkout go<vernum> for go version just fetched'
